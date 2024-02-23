9d7f2e1729e638f89b9985126228d2f48c6adbce
Doug Shook <dshook@wustl.edu>
2024-02-23 15:56:56 -0600

    Dice refactored with functions

--- src/naming/Dice.java
+++ src/naming/Dice.java
@@ -2,39 +2,75 @@ package naming;
 
 public class Dice {
 
+	
     public static void main(String[] args) {
         int dice = 4;
         int throwss = 7;
+ 
+        int[] rollFrequencyTable = throwManyTimes(dice, throwss);
+        printFrequencies(rollFrequencyTable);
 
-        int[][] rollTracker = new int[throwss][dice];
-        int[] rollFrequencyTable = new int[dice * 6];
+    }
 
+    /**
+     * Prints out the contents of the frequency table
+     * @param rollFrequencyTable 
+     */
+	public static void printFrequencies(int[] rollFrequencyTable) {
+		for(int i = 0; i < rollFrequencyTable.length; i++) {
+            System.out.println("Sum: " + i + " times: " + rollFrequencyTable[i]);
+        }
+	}
+
+	/**
+	 * Rolls the given number of dice, throws number of times
+	 * @param dice number of dice
+	 * @param throwss number of throws
+	 * @return a table of how often each sum was rolled
+	 */
+	public static int[] throwManyTimes(int dice, int throwss) {
+		int[] rollFrequencyTable = new int[dice * 6];
+		int[][] rollTracker = new int[throwss][dice];
         int sumOfThrow = 0;
         int yahtzees = 0;
         for(int i = 0; i < throwss; i++) {
-        	sumOfThrow = 0;
-            System.out.print("Roll: ");
-            for(int j = 0; j < dice; j++) {
-                int dieRoll = (int)(Math.random() * 6 + 1);
-                rollTracker[i][j] = dieRoll;
-                sumOfThrow += dieRoll;
-                System.out.print(dieRoll + " ");
-            }
-            System.out.println(" Sum: " + sumOfThrow);
+        	sumOfThrow = computeAndPrintSum(dice, rollTracker, i);
 
-            for(int j = 0; j < dice - 1; j++) {
-                if(rollTracker[i][j] == rollTracker[i][j+1]) {
-                    yahtzees++;
-                }
+            if(checkForYahtzees(dice, rollTracker, i)) {
+            	yahtzees++;
             }
             rollFrequencyTable[sumOfThrow]++;
         }
         System.out.println("There were " + yahtzees + " yahtzees");
+		return rollFrequencyTable;
+	}
 
-        for(int i = 0; i < rollFrequencyTable.length; i++) {
-            System.out.println("Sum: " + i + " times: " + rollFrequencyTable[i]);
+	private static int computeAndPrintSum(int dice, int[][] rollTracker, int i) {
+		int sumOfThrow;
+		sumOfThrow = rollDice(dice, rollTracker, i);
+		System.out.println(" Sum: " + sumOfThrow);
+		return sumOfThrow;
+	}
+
+	private static int rollDice(int dice, int[][] rollTracker, int throwNumber) {
+		int sumOfThrow = 0;
+		System.out.print("Roll: "); 
+		for(int j = 0; j < dice; j++) { 
+		    int dieRoll = (int)(Math.random() * 6 + 1); 
+		    rollTracker[throwNumber][j] = dieRoll; 
+		    sumOfThrow += dieRoll;
+		    System.out.print(dieRoll + " ");
+		}
+		return sumOfThrow;
+	}
+    
+    public static boolean checkForYahtzees(int dice, int[][] rollTracker, int throwNumber) {
+    	for(int j = 0; j < dice - 1; j++) {
+            if(rollTracker[throwNumber][j] != rollTracker[throwNumber][j+1]) {
+                return false;
+            }
         }
-
+    	return true;
     }
 
 }
